20657857b21b1e438b33f6bf94ccf318df71c3fd
lazysimon <44077948+lazysimon@users.noreply.github.com>
2019-08-23 15:25:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,25 +5,45 @@ from utils import *
 
 if __name__ == '__main__':
 	### remember to change test to train if train is needed 
-	filename = 'https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/master/data/test.csv'
-	df = pd.read_csv(filename)
-	df.drop('Id', axis = 1, inplace = True)
-	interest_df = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/interest.csv')
+	name = 'test'
+	filename = 'https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/master/data/' + name + '.csv'
+	dataset = pd.read_csv(filename)
+
+	### drop the ID column
+	dataset.drop('Id', axis = 1, inplace = True)
+
+	### the interest rate from 2006 to 2010
+	interest = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/interest.csv')
+	### housing price index in Ames
 	ameshpi = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/ameshpi.csv')
-	sch_rank = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/SchoolRanking.csv')
+	schoolrank = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/SchoolRanking.csv')
 
-	df = ImputeMissingValue(df)
-	df = FeatureEngineering(df)
-	df = AddInterest(interest_df, df)
-	df = SchoolRank(sch_rank, df)
-	df.drop(['Unnamed: 0'], axis = 1, inplace = True)
-	df_dummified = dummify(df)
+	# impute the missing values in the model
+	dataset = ImputeMissingValue(dataset)
 
-	if 'SalePrice' in list(df.columns):
-		saleprice = AddHPI(ameshpi, df)
+	# complete the feature engineering
+	dataset = FeatureEngineering(dataset)
+
+	# add the interest rate
+	dataset = AddInterest(interest, dataset)
+
+	# add the school rank to the dataset
+	dataset = SchoolRank(schoolrank, dataset)
+	
+	# drop the unnecessary columns
+	dataset.drop(['Unnamed: 0'], axis = 1, inplace = True)
+	
+	# dummify the variables
+	dataset_dummified = Dummify(dataset)
+
+	# Check missing values
+	CheckMissing(dataset)
+
+	if 'SalePrice' in list(dataset.columns):
+		saleprice = AddHPI(ameshpi, dataset)
 		#df.drop(['SalePrice'], axis=1, inplace = True)
 		saleprice.to_csv(r'./data/price.csv', index = False)
 
-	df.to_csv(r'./data/test.csv', index = False)
-	df_dummified.to_csv(r'./data/test_dummified.csv', index = False)
+	dataset.to_csv(r'./data/'+ name + '.csv', index = False)
+	dataset_dummified.to_csv(r'./data/'+ name + '_dummified.csv', index = False)
 		
