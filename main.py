ec4e14221b46f6100a6df3c27c2251ea6bc451bc
lazysimon <44077948+lazysimon@users.noreply.github.com>
2019-08-23 17:04:29 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import re
 from utils import *
 
 if __name__ == '__main__':
-	### remember to change test to train if train is needed 
-	name = 'test'
-	filename = 'https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/master/data/' + name + '.csv'
-	dataset = pd.read_csv(filename)
+	### housing price index in Ames
+	schoolrank = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/SchoolRanking.csv')
+	ameshpi = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/ameshpi.csv')
+	### the interest rate from 2006 to 2010
+	interest = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/interest.csv')
+	train = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/master/data/train.csv')
+	test = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/master/data/test.csv')
+	saleprice = AddHPI(ameshpi, train)
+
+	train.drop(['SalePrice', 'Qrtr'], axis = 1, inplace = True)
+
+	dataset = pd.concat([train,test], axis = 0)
 
 	### drop the ID column
 	dataset.drop('Id', axis = 1, inplace = True)
-
-	### the interest rate from 2006 to 2010
-	interest = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/interest.csv')
-	### housing price index in Ames
-	ameshpi = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/ameshpi.csv')
-	schoolrank = pd.read_csv('https://raw.githubusercontent.com/TiGaI/HousingPriceKaggleProject3/xiangwei/data/SchoolRanking.csv')
 
 	# impute the missing values in the model
 	dataset = ImputeMissingValue(dataset)
@@ -39,11 +41,18 @@ if __name__ == '__main__':
 	# Check missing values
 	CheckMissing(dataset)
 
-	if 'SalePrice' in list(dataset.columns):
-		saleprice = AddHPI(ameshpi, dataset)
-		#df.drop(['SalePrice'], axis=1, inplace = True)
-		saleprice.to_csv(r'./data/price.csv', index = False)
+	train_df = dataset.iloc[0:1460]
 
-	dataset.to_csv(r'./data/'+ name + '.csv', index = False)
-	dataset_dummified.to_csv(r'./data/'+ name + '_dummified.csv', index = False)
+	test_df = dataset.iloc[1460:2919]
+
+
+	train_dummified = dataset_dummified.iloc[0:1461]
+
+	test_dummified = dataset_dummified.iloc[1461:2919]
+
+	saleprice.to_csv(r'./data/price.csv', index = False)
+	train_df.to_csv(r'./data/train_df.csv', index = False)
+	test_df.to_csv(r'./data/test_df.csv', index = False)
+	train_dummified.to_csv(r'./data/train_dummified.csv', index = False)
+	test_dummified.to_csv(r'./data/test_dummified.csv', index = False)
 		
